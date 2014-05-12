9f78f2fd63bd2581cb79ef892be04c910bae4ad0
Alexey Morozov <zackflame@gmail.com>
2014-05-12 08:30:27 +0400

    Custom syntax for Java object member access

--- PrologTest/res/utest.pl
+++ PrologTest/res/utest.pl
@@ -1,1 +1,30 @@
-java_test(X, Y) :- jpl_call(X, 'bar', [2], Y).
+:- op(100, yfx, user:(#)).
+
+bind_vars(_, []).
+bind_vars(AVs, [A = V|T]) :-
+	member(A = V1, AVs) -> V = V1,
+	bind_vars(AVs, T).
+
+java_eval(Object#Member, Result) :-
+	functor(Member, _, _),
+	Member =.. [Name|Args],
+	meta(Args, Args1),
+	jpl_call(Object, Name, Args1, Result).
+
+meta(T, T) :- var(T), !, print('v: '), print(T), nl.
+meta(T, T) :- atomic(T), !, print('a: '), print(T), nl.
+meta([Term|T], [Term1|T1]) :-
+	(nonvar(Term), java_eval(Term, Term1) ; meta(Term, Term1)), !,
+	meta(T, T1).
+meta(Term, Term1) :-
+	print('meta: '), print(Term), nl,
+	Term =.. [Name|Args],
+	meta(Args, Args1),
+	Term1 =.. [Name|Args1],
+	print('meta> '), print(Term1), nl.
+	
+java_test(E, S, Y) :-
+	read_term_from_atom(E, T, [variable_names(AVs)]),
+	bind_vars(['S' = S, 'Y' = Y], AVs),
+	meta(T, T1),
+	T1.
--- PrologTest/src/ru/eltech/utest/predicate/Program.java
+++ PrologTest/src/ru/eltech/utest/predicate/Program.java
@@ -5,7 +5,6 @@ import java.util.Hashtable;
 import jpl.Atom;
 import jpl.Compound;
 import jpl.JPL;
-import jpl.JRef;
 import jpl.Query;
 import jpl.Term;
 import jpl.Variable;
@@ -18,14 +17,15 @@ public final class Program {
 		}
 	}
 	
+	@SuppressWarnings("rawtypes")
 	public static void main(String[] args) {
 		Query query = new Query("consult", new Term[] { new Atom("utest.pl") });
 		System.out.println("consult " + (query.hasSolution() ? "succeeded" : "failed"));
 		
 		Variable Y = new Variable("Y");
-		Term term = new Compound("java_test", new Term[] { JPL.newJRef(new Foo()), Y });
-		System.out.println(term.toString());
-		Query q = new Query(term);
+		Term goal = new Compound("java_test", new Term[] { new Atom("Y = S#bar(2)"), JPL.newJRef(new Foo()), Y });
+		System.out.println("Goal: " + goal.toString());
+		Query q = new Query(goal);
 		while (q.hasMoreSolutions()) {
 			Hashtable solution = q.nextSolution();
 			System.out.println("Y = " + solution.get(Y.name));
